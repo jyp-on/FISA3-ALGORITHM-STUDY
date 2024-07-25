f439283181ee07faa450ba2e30c4a68771c44c1a
jyp-on <okmlnsunok@naver.com>
2024-07-25 14:37:31 +0900

    백준 : DNA비밀번호

---
+++ Baekjoon/jyp-on/P12891_DNA비밀번호_A.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P12891_DNA비밀번호_A {
+    static Map<Character, Integer> input = new HashMap<>();
+    static Map<Character, Integer> target = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        char[] str = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+        int answer = 0;
+
+        target.put('A', Integer.parseInt(st.nextToken()));
+        target.put('C', Integer.parseInt(st.nextToken()));
+        target.put('G', Integer.parseInt(st.nextToken()));
+        target.put('T', Integer.parseInt(st.nextToken()));
+
+        for(int i=0; i<P; i++) {
+            input.put(str[i], input.getOrDefault(str[i], 0) + 1);
+        }
+
+        int p1 = 0, p2 = P;
+        if(check()) answer++;
+
+        while(p2 < S) {
+            input.put(str[p1], input.get(str[p1])-1);
+            if(input.get(str[p1]) == 0) input.remove(str[p1]);
+            p1++;
+
+            input.put(str[p2], input.getOrDefault(str[p2], 0) + 1);
+            p2++;
+
+            if(check()) answer++;
+        }
+
+        System.out.println(answer);
+    }
+
+    static boolean check() {
+        for(char c : target.keySet()) {
+            if(input.getOrDefault(c, 0) < target.get(c)) return false;
+        }
+        return true;
+    }
+}
---
+++ Baekjoon/jyp-on/P12891_DNA비밀번호_B.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P12891_DNA비밀번호_B {
+
+    static int[] count = new int[26];
+
+    // 하나라도 1 이상이면 false
+    static boolean check() {
+        return !(count[0] > 0 || count[2] > 0 || count[6] > 0 || count[19] > 0);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        char[] str = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+
+        count[0] = Integer.parseInt(st.nextToken());
+        count[2] = Integer.parseInt(st.nextToken());
+        count[6] = Integer.parseInt(st.nextToken());
+        count[19] = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        for(int i=0; i<P; i++) {
+            count[str[i]-'A']--;
+        }
+
+        if(check()) ans++;
+
+        for(int i=0; i<S-P; i++) {
+            count[str[i]-'A']++;
+            count[str[i+P]-'A']--;
+
+            if(check()) ans++;
+        }
+
+        System.out.println(ans);
+    }
+}

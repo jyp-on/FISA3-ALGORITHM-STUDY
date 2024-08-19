f1554454a64f060949ce42b59c49f6c3f3b17a4b
jyp-on <okmlnsunok@naver.com>
2024-08-19 11:48:41 +0900

    백준 : N과 M 1~#

--- Baekjoon/jyp-on/week7/P15651_N과M3.java
+++ Baekjoon/jyp-on/week7/P15651_N과M3.java
@@ -1,15 +1,14 @@
 package week7;
 
-import java.util.*;
-public class P15649_N과M1 {
+import java.util.Scanner;
+
+public class P15651_N과M3 {
     static int N, M;
-    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        visited = new boolean[N + 1];
         int[] out = new int[M];
         combination(out, 0);
         System.out.println(sb.toString());
@@ -25,12 +24,8 @@ public class P15649_N과M1 {
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                out[depth] = i;
-                combination(out, depth + 1);
-                visited[i] = false;
-            }
+            out[depth] = i;
+            combination(out, depth + 1);
         }
     }
 }
